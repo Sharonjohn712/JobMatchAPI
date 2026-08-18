47556a6357ff33331de3dc2b06b04bdef9101c45
Sharon Edward John <sharonjohn712@gmail.com>
2026-08-18 16:35:08 -0400

    Resume and Job entities

--- src/main/java/com/jobapi/app/entity/Resume.java
+++ src/main/java/com/jobapi/app/entity/Resume.java
@@ -1,35 +1,29 @@
 package com.jobapi.app.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Resume {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String candidateName;
 
+    @NotBlank
+    @Column(length = 2000)
     private String skills;
-
-    // getters/setters
-    public String getCandidateName() {
-        return candidateName;
-    }
-
-    public void setCandidateName(String candidateName) {
-        this.candidateName = candidateName;
-    }
-
-    public String getSkills() {
-        return skills;
-    }
-
-    public void setSkills(String skills) {
-        this.skills = skills;
-    }
-
 }
